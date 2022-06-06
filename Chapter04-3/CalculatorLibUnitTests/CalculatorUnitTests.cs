a99b13f433cc2bf61efb57a6049d13c39598fb74
seren01 <96581254+seren01@users.noreply.github.com>
2022-06-06 04:25:10 +0300

    Add files via upload

---
+++ Chapter04-3/CalculatorLibUnitTests/CalculatorUnitTests.cs
@@ -0,0 +1,41 @@
+using Packt;
+using System;
+using Xunit;
+
+namespace CalculatorLibUnitTests
+{
+    public class CalculatorLibUnitTests
+    {
+        [Fact]
+        public void TestAdding2And2()
+        {
+            // размещение
+            double a = 2;
+            double b = 2;
+            double expected = 4;
+            var calc = new Calculator();
+
+            // действие
+            double actual = calc.Add(a, b);
+
+            // утверждение
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestAdding2And3()
+        {
+            // размещение
+            double a = 2;
+            double b = 3;
+            double expected = 5;
+            var calc = new Calculator();
+
+            // действие
+            double actual = calc.Add(a, b);
+            
+            // утверждение
+            Assert.Equal(expected, actual);
+        }
+    }
+}
